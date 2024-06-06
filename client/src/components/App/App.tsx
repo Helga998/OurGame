88b7fb24217c9ae22e744ec418f1575a5bbfa856
Helga998 <bom1993@yandex.ru>
2024-06-06 14:37:57 +0300

    dev created

---
+++ client/src/components/App/App.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App(): JSX.Element {
+  const [count, setCount] = useState(0);
+
+  return (
+  <div>
+    Hello
+  </div>
+  );
+}
+
+export default App;
