be97987b7afbb834379f80ae2d890f7e4dab819c
irinacova <kovalenko.23@inbox.ru>
2024-06-07 15:28:29 +0300

    feat\superstyle

--- client/src/components/dogs/DogCards.tsx
+++ client/src/components/dogs/DogCards.tsx
@@ -38,17 +38,17 @@ function DogCards() {
     <>
       <div className="d-flex justify-content-center">
         <div className="row ">
-          <img src={dog.img} alt="" width="1000px" height="800px" />
-          <input 
-          width='200px'
-          
+          <img src={dog.img} alt="" width="300px" height="800px" />
+          <input
+            style={{ fontSize: '30px' }}
+            width="200px"
             placeholder="Введи ответ"
             value={answer}
             onChange={(e) => setAnswer(e.target.value)}
             disabled={rightAnswer ? true : false}
           ></input>
           {rightAnswer ? (
-            <button className="btn btn-outline-success">
+            <button className="btn btn-outline-success" style={{ fontSize: '30px' }}>
               <Link
                 className="link-underline-light link-body-emphasis link-offset-2 link-underline-opacity-25 link-underline-opacity-75-hover fw-bold"
                 to={`/dogs/${+id + 1}`}
@@ -58,7 +58,8 @@ function DogCards() {
             </button>
           ) : (
             <button
-              width="40px"
+              style={{ fontSize: '30px' }}
+              width="80px"
               height="50px"
               className="btn btn-outline-warning link-body-emphasis link-offset-2 link-underline-opacity-25 link-underline-opacity-75-hover fw-bold"
               class="btn btn-dark"
